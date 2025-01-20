4376b7defdbe6280941338dd0730af7c091d895e
AlejandroFreitasSanchez <freitassanchezalejandro@gmail.com>
2025-01-20 19:18:45 +0100

    Creación plantilla Spring boot.

---
+++ BCNC/src/main/java/com/bcnc/alejandro/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bcnc.alejandro;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BcncApplication.class);
+	}
+
+}
