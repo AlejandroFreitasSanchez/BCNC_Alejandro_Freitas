23ee5eb33f1ab2f15e8a1d39db5af2261646efa0
AlejandroFreitasSanchez <freitassanchezalejandro@gmail.com>
2025-01-22 19:35:02 +0100

    Desarrollo de las capas del proyecto y base de datos en h2, además de creación de test.

---
+++ BCNC/src/main/java/com/bcnc/alejandro/domain/model/Price.java
@@ -0,0 +1,22 @@
+package com.bcnc.alejandro.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Price {
+	private Long productId;
+	private Long brandId;
+	private Integer priceList;
+	private LocalDateTime startDate;
+	private LocalDateTime endDate;
+	private int priority;
+	private BigDecimal price;
+	private String currency;
+
+}
