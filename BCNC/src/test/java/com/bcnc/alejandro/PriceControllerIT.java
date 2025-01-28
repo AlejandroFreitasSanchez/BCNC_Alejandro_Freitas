c6498476e68594d0fb6af906f32c59f2add738c4
AlejandroFreitasSanchez <freitassanchezalejandro@gmail.com>
2025-01-28 17:48:49 +0100

    Fix: Cambio del nombre PriceControllerIntegrationTest a PriceControllerIT.

--- BCNC/src/test/java/com/bcnc/alejandro/PriceControllerIT.java
+++ BCNC/src/test/java/com/bcnc/alejandro/PriceControllerIT.java
@@ -16,7 +16,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PriceControllerIntegrationTest {
+public class PriceControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
